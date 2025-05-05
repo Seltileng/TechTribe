a7748ef8430c4df6bd81e4b53d8ed2caecf37679
seltileng <seltilengdabuk@gmail.com>
2025-05-05 07:15:57 +0100

    adds a 404 page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.registration-link');
+const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('btnlogin-popup')
 const iconClose = document.querySelector('.icon-close')
 
@@ -19,3 +19,47 @@ btnPopup.addEventListener('click', ()=> {
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
 })
+
+
+
+const subscribeEmail = document.querySelector('.btn-btn-sub')
+
+function subscribeEmail(){
+    console.log("SUCESSFULLY SUBCRIBED YOUR EMAIL")
+}
+
+subscribeEmail.addEventListener('click')
+
+function entered(){
+    alert("LOGGED IN SUCCESSFULLY")
+}
+
+
+
+function displayMessage(){
+    alert("successful")
+}
+
+function signupMessage(){
+    alert("SIGN UP SUCCESSFUL")
+}
+
+function loginMessage(){
+    alert("LOG IN SUCESSFUL")
+}
+
+
+
+
+function loginCred(){ if(user) { 
+    window.location.href = "login.html"
+
+}
+else{
+    console.log("HTTP REQUEST REJECTED")
+}
+}
+
+
+
+function 
